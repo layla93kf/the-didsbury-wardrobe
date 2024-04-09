1ee29eebf57de288c0b52a753ecb2547149be5a0
layla93k <layla.kawafi@hotmail.com>
2024-04-09 19:11:49 +0100

    chore: setting colour scheme

--- frontEnd/tailwind.config.js
+++ frontEnd/tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   content: ["./src/**/*.{html,js, jsx}"],
   mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#ffffff",
+        lightGrey: "#f8f8f2",
+        darkGrey: "#c0c0c0",
+        black: "#0d0d0c",
+      },
+    },
   },
   plugins: [],
 };
