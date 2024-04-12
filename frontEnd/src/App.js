b914d3498230d86edcc221e2524b6b135e97a4c7
layla93k <layla.kawafi@hotmail.com>
2024-04-12 19:42:31 +0100

    feat: setting up component structure

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,5 +1,16 @@
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import Header from "./components/Header";
+import Navbar from "./components/NavBar";
+
 function App() {
-  return <div className="App"></div>;
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px");
+  return (
+    <div className="app white">
+      <Header />
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontEnd/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header" id="flex-div">
+      <h3>Welcome to the Didsbury Wardrobe</h3>
+      <h1 class="font-serif font-semibold text-4xl">The Didsbury Wardrobe</h1>
+    </header>
+  );
+}
